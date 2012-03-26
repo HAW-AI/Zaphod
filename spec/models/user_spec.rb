db20987b97ca999903a303b80d09ba1f6a6836ed
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-03-26 15:18:51 +0200

    user needs username and email to be valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe User do
   it { should_not allow_mass_assignment_of :created_at }
   it { should_not allow_mass_assignment_of :updated_at }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
 end
