c5875c6c8342501af8b805d76f3275f6407edbbe
Ben <salzig+github@gmail.com>
2012-04-04 22:09:52 +0200

    basic user 'registration'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,4 @@
 class UsersController < ResourceController
+  before_filter :authenticate_user!, :except => :create
+  actions :create
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe :create do
+    def do_post(data={})
+      post :create, data.merge(format: :json)
+    end
+    
+    context "given valid data" do
+      let(:user) { FactoryGirl.attributes_for(:user).extract!(:username, :email, :password) }
+      before :each do do_post :user => user end
 
+      it { response.should be_success }
+    end
+  end
 end
