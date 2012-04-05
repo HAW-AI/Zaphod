c90058f48bcb23d54e11f71a54caf05b27f3c844
Till Theis <till_theis2@yahoo.de>
2012-04-05 14:01:27 +0200

    add uniqueness specs for username and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,12 @@ describe User do
   it { should_not allow_mass_assignment_of :updated_at }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
+
+  context "uniqueness" do
+    before { FactoryGirl.create(:user) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:username) }
+  end
   
   context "given a valid user" do
     subject { FactoryGirl.create(:user) }
