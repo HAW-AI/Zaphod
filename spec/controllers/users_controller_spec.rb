6c1041bb1877a7e4f47f226c3698b78bba0f6e10
Till Theis <till_theis2@yahoo.de>
2012-04-11 19:07:20 +0200

    test for db record creation

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,9 +8,15 @@ describe UsersController do
     
     context "with valid data" do
       let(:user) { FactoryGirl.attributes_for(:user).extract!(:username, :email, :password) }
+      let!(:num_users) { User.count }
+
       before :each do do_post user: user end
 
       it { response.should be_success }
+
+      it "creates a new db record" do
+        User.count.should == num_users + 1
+      end
     end
     
     context "with existing user data" do
