4ef5deb3ef6895e97f126990e54d05007b45a013
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-04-23 16:08:55 +0200

    fixed user factory with sequences to match unique constraint

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,11 @@
 
 FactoryGirl.define do
   factory :user do
-    username "Robert'); DROP TABLE Students;--"
+    sequence(:username) { |n| "Robert#{n}'); DROP TABLE Students;--" }
     sequence(:email) { |n| "zaphod.#{n}@example.com" }
     password "bobby tables"
     password_confirmation "bobby tables"
     salt "tables"
-    authentication_token "valid_auth_token"
+    sequence(:authentication_token) { |n| "valid_auth_token#{n}" }
   end
 end
