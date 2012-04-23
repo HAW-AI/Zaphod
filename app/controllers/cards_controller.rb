e99553d0a14ec41be60e2372dffe006d5c7b6f41
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-04-23 17:30:13 +0200

    non working cardcontroller spec

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,10 @@
 class CardsController < ResourceController
+  belongs_to :deck
+
+  def create
+    debugger.log parent?
+    @card = @deck.cards.build
+    @card.user = current_user
+    create!
+  end
 end
--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe CardsController do
   describe :create do
     def do_post(data = {})
-      puts "data: " + data.to_s
+      puts "do_post: data: " + data.to_s
       post :create, data.merge(format: :json)
     end
 
@@ -11,28 +11,20 @@ describe CardsController do
       context "with valid data" do
         let(:data) do
           # create dependent db entries (deck and user)
-          card = FactoryGirl.build(:card)
+          card = FactoryGirl.create(:card)
+          data = {}
 
-          data = FactoryGirl.attributes_for(:card)
-          data[:auth_token] = FactoryGirl.attributes_for(:user)[:authentication_token]
-          data[:deck] = card.deck.id
-          data[:user] = card.deck.user_id
+          data[:card] = card.attributes.except("deck_id", "id", "user_id", "created_at", "updated_at")
+          data[:auth_token] = card.user.authentication_token
           data
         end
 
         before { do_post data }
 
-        specify do
-          puts "!!!"
-          puts response.message
-          puts response.body
-          true
-        end
-
         specify { response.should be_success }
 
         it "should create a db record" do
-          expect { do_post data }.to change(User, :count).by(1)
+          expect { do_post data }.to change(Card, :count).by(1)
         end
       end
     end
