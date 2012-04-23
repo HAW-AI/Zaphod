deafe6b1aacd97ea87906c2075ba7885e5ed0cf8
Ben <salzig+github@gmail.com>
2012-04-24 00:02:44 +0200

    add cards controller with create spec

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,10 @@
 class CardsController < ResourceController
+	belongs_to :deck
+	action :create
+
+	def create
+		@card = Card.new(params[:card])
+		@card.deck = @deck
+		create!
+	end
 end
--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -2,39 +2,25 @@ require 'spec_helper'
 
 describe CardsController do
   describe :create do
-    def do_post(data = {})
-      puts "data: " + data.to_s
-      post :create, data.merge(format: :json)
+    let(:deck) { FactoryGirl.create(:deck) }
+    let(:card) { FactoryGirl.build(:card, deck: deck) }
+
+    def do_create(data = {})
+      post :create, data.merge(format: :json, deck_id: deck.id)
     end
 
-    context "as an authenticated user" do
-      context "with valid data" do
-        let(:data) do
-          # create dependent db entries (deck and user)
-          card = FactoryGirl.build(:card)
-
-          data = FactoryGirl.attributes_for(:card)
-          data[:auth_token] = FactoryGirl.attributes_for(:user)[:authentication_token]
-          data[:deck] = card.deck.id
-          data[:user] = card.deck.user_id
-          data
-        end
-
-        before { do_post data }
-
-        specify do
-          puts "!!!"
-          puts response.message
-          puts response.body
-          true
-        end
-
-        specify { response.should be_success }
-
-        it "should create a db record" do
-          expect { do_post data }.to change(User, :count).by(1)
-        end
+    context "with valid data" do
+      let(:params) do
+        {
+          card: card.attributes.slice(Card.accessible_attributes),
+          auth_token: card.user.authentication_token
+        }
       end
+
+      before { do_create params }
+
+      specify { response.should be_success }
+      it { expect { do_create params }.to change(Card, :count).by(1) }
     end
   end
 end
