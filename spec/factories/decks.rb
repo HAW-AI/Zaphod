40744cb308dc388526ee1b150ea55266406ea03a
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-04-23 16:09:34 +0200

    fixed decks factory

--- spec/factories/decks.rb
+++ spec/factories/decks.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :deck do
-    user_id 1
+    user
     title "Flux capacitor"
     description "The flux capacitor, which consists of a regularly squared compartment with three flashing Geissler style-tubes (arranged in a \"Y\" configuration), is described by Doc as \"what makes time travel possible.\" The device is the core component of the time machine."
   end
