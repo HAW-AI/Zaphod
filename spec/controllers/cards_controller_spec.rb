0388f4a538c8ef489e2ac06176e729199494a20a
Ben <salzig+github@gmail.com>
2012-04-24 13:14:09 +0200

    refactor do_create and do_update into helper

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -2,23 +2,18 @@ require 'spec_helper'
 
 describe CardsController do
   let(:deck) { FactoryGirl.create(:deck) }
+  let(:params) do
+    {
+      card: card.attributes.slice(Card.accessible_attributes),
+      auth_token: card.user.authentication_token
+    }
+  end
 
   describe :create do
     let(:card) { FactoryGirl.build(:card, deck: deck) }
 
-    def do_create(data = {})
-      post :create, data.merge(format: :json, deck_id: deck.id)
-    end
-
     context "with valid data" do
-      let(:params) do
-        {
-          card: card.attributes.slice(Card.accessible_attributes),
-          auth_token: card.user.authentication_token
-        }
-      end
-
-      before { do_create params }
+      before { do_create params.merge(format: :json, deck_id: deck.id) }
 
       specify { response.should be_success }
       it { expect { do_create params }.to change(Card, :count).by(1) }
@@ -28,18 +23,7 @@ describe CardsController do
   describe :update do
     let(:card) { FactoryGirl.create(:card, deck: deck) }
 
-    def do_update(data={})
-      post :update, data.merge(format: :json, id: card.id, deck_id: card.deck.id)
-    end
-
-    let(:params) do
-      {
-        card: card.attributes.slice(Card.accessible_attributes),
-        auth_token: card.user.authentication_token
-      }
-    end
-
-    before { do_update params }
+    before { do_update params.merge(format: :json, id: card.id, deck_id: card.deck.id) }
 
     specify { response.should be_success }
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  
+
   config.include Devise::TestHelpers, :type => :controller
+  config.include RequestHelpers, :type => :controller
   config.extend LoginMacros, :type => :controller
 end
