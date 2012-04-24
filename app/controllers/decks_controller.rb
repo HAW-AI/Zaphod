c1a8fd3763cd796f2d130906c392574130fba100
Till Theis <till_theis2@yahoo.de>
2012-04-24 15:13:28 +0200

    nest decks and cards under users

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,10 @@
 class DecksController < ResourceController
+	belongs_to :user
+	action :create, :update
+
+	def create
+		@deck = Deck.new(params[:deck])
+		@deck.user = @user
+		create!
+	end
 end
