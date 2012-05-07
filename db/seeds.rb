3c86fbdb7177919010d1c8f20d9b54f1d401a56a
Till Theis <till_theis2@yahoo.de>
2012-05-07 17:09:34 +0200

    create seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+if Rails.env == "development"
+  FactoryGirl.create :card
+end
