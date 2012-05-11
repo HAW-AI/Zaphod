0cf3fe104bf62ae83368531671ed5c81d4b19267
Till Theis <till_theis2@yahoo.de>
2012-05-11 19:13:20 +0200

    refactor validation specs

--- spec/javascripts/models/card_spec.js
+++ spec/javascripts/models/card_spec.js
@@ -1,25 +1,13 @@
 describe('Card', function() {
-  var card;
-
-  beforeEach(function() {
-  	card = new Zaphod.Card();
-  });
-
   describe('without front text', function() {
     it('is invalid', function() {
-      var spy = jasmine.createSpy();
-      card.bind('error', spy);
-      card.set({ front: '' });
-      expect(spy).toHaveBeenCalled();
+      expect(new Zaphod.Card({ front: '' }).isValid()).toBe(false);
     });
   });
 
   describe('without back text', function() {
     it('is valid', function() {
-      var spy = jasmine.createSpy();
-      card.bind('error', spy);
-      card.set({ back: '' });
-      expect(spy).not.toHaveBeenCalled();
+      expect(new Zaphod.Card({ back: '' }).isValid()).toBe(true);
     });
   });
 });
