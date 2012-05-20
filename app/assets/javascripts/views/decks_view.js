38a1fdcc50126fa222f03e5175537e0beb64a408
Till Theis <till_theis2@yahoo.de>
2012-05-20 16:01:11 +0200

    remove unneeded code

--- app/assets/javascripts/views/decks_view.js
+++ app/assets/javascripts/views/decks_view.js
@@ -11,17 +11,8 @@ Zaphod.DecksView = Backbone.View.extend({
   },
 
   render: function() {
-    console.log(this.collection.invoke('toJSON'))
     var data = { decks: this.collection.invoke('toJSON') };
     this.$el.html(this.template(data, this._helpers));
     return this;
-  },
-
-  _helpers: {  
-    link_to: function(txt) {
-      console.log(arguments)
-      var str = '<a href="' + this.collection.get(obj.id) + '">"' + txt + '</a>';
-      return new Handlebars.SafeString(str);
-    }
   }
 });
