9a2deb09a6b988f31ef8f1d32b04a08201921a0e
Till Theis <till_theis2@yahoo.de>
2012-05-23 00:45:38 +0200

    show cards within deck view

--- app/assets/javascripts/views/deck_view.js
+++ app/assets/javascripts/views/deck_view.js
@@ -5,10 +5,17 @@ Zaphod.DeckView = Backbone.View.extend({
     _.bindAll(this, 'render');
 
     this.model.bind('change', this.render);
+
+    this.cards = new Zaphod.Cards({ deckId: this.model.get('id') });
+    this.cards.fetch();
+    this.cardsView = 
+
+    this.render();
   },
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
+    new Zaphod.CardsView({ collection: this.cards, el: this.$('.cards')[0] });
     return this;
   }
 });
