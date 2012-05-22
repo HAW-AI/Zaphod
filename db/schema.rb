258b72273af10c67f7d1510d0d00dd0466e060dc
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-05-22 08:49:51 +0200

    dropped devise users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120519193905) do
+ActiveRecord::Schema.define(:version => 20120522064822) do
 
   create_table "cards", :force => true do |t|
     t.text     "front"
@@ -44,36 +44,5 @@ ActiveRecord::Schema.define(:version => 20120519193905) do
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
   end
-
-  create_table "users", :force => true do |t|
-    t.string   "username",                                     :null => false
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
-    t.string   "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-    t.string   "email",                        :default => "", :null => false
-    t.string   "encrypted_password",           :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "authentication_token"
-    t.integer  "failed_attempts",              :default => 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-  end
-
-  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
-  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
