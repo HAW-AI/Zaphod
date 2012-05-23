5d210f9f851bdd9207435e51a07cf27b95b9ace6
Till Theis <till_theis2@yahoo.de>
2012-05-23 23:55:17 +0200

    make tests work with new auth tokens

--- spec/javascripts/spec.js
+++ spec/javascripts/spec.js
@@ -2,6 +2,9 @@
 //= require_tree .
 
 beforeEach(function() {
+  Zaphod.currentUser.set('authToken', 'ZPYxqZ9tN2Apy9ZMxTHE');
+
+
   this.addMatchers({
     toBeValid: function() {
       return this.actual.isValid();
