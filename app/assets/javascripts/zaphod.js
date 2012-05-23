695085413cff840a78bc6953d38b27b797f391bd
Till Theis <till_theis2@yahoo.de>
2012-05-24 00:48:21 +0200

    correctly re-trigger the current route after login/logout

--- app/assets/javascripts/zaphod.js
+++ app/assets/javascripts/zaphod.js
@@ -34,7 +34,10 @@ Backbone.sync = function(method, model, options) {
 
     // re-trigger current route to make use of new privileges
     Zaphod.currentUser.bind('change', function() {
-      Zaphod.router.navigate(Backbone.history.fragment, { trigger: true, replace: true });
+      var frag = Backbone.history.fragment;
+      Backbone.history.fragment =
+        'random string to force the router to re-trigger the current route';
+      Zaphod.router.navigate(frag, { trigger: true, replace: true });
     });
 
     Backbone.history.start({ pushState: true });
