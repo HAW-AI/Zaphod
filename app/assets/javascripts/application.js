a78a85492d036fb313a1168f917c6085560919f8
Till Theis <till_theis2@yahoo.de>
2012-05-30 22:57:57 +0200

    new main.js to run when everything else is loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,4 @@
 //= require_tree ../templates
 //= require_tree ./views
 //= require router
+//= require main
--- app/assets/javascripts/zaphod.js
+++ app/assets/javascripts/zaphod.js
@@ -28,29 +28,3 @@ Backbone.sync = function(method, model, options) {
 
   return backboneSync(method, model, options);
 };
-
-(function($) {
-  $(document).ready(function() {
-    Zaphod.router = new Zaphod.Router();
-
-    Zaphod.currentUser = new Zaphod.CurrentUser();
-    new Zaphod.CurrentUserView({ model: Zaphod.currentUser, el: $('#current_user') }).render();
-
-    // re-trigger current route to make use of new privileges
-    //Zaphod.currentUser.bind('change', function() {
-    //  var frag = Backbone.history.fragment;
-    //  Backbone.history.fragment =
-    //    'random string to force the router to re-trigger the current route';
-    //  Zaphod.router.navigate(frag, { trigger: true, replace: true });
-    //});
-
-    // prevent links from making a big request
-    $(document).on('click', 'a', function (ev) {
-      var href = $(this).attr('href');
-      ev.preventDefault();
-      Zaphod.router.navigate(href, true);
-    });
-
-    Backbone.history.start({ pushState: true });
-  });
-})(jQuery);
