8a1e545b0498cb093b55bb274d4fa2ec623a0142
Till Theis <till_theis2@yahoo.de>
2012-06-14 19:17:23 +0200

    only allow owner, editor or viewer roles

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -3,14 +3,9 @@ class Collaborator < ActiveRecord::Base
   belongs_to :user
 
   validates :deck, :user, :role, presence: true
-  validate :role_in_roles
 
   attr_accessible :deck_id, :user_id, :role
 
   ROLES = %w(owner editor viewer)
-
-  private
-  def role_in_roles
-    ROLES.include?(role)
-  end
+  validates_inclusion_of :role, :in => ROLES
 end
--- spec/models/collaborator_spec.rb
+++ spec/models/collaborator_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Collaborator do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:deck) { FactoryGirl.create(:deck) }
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "should only allow owner, editor or viewer roles" do
+    %w(owner editor viewer).each do |role|
+      should allow_value(role).for(:role)
+    end
+
+    subject { Collaborator.new }
+    subject.user = user
+    subject.deck = deck
+
+    should_not allow_value("foo").for(:role)
+  end
 end
